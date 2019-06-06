2d4725f128c9d92b0516ec49cc062ccf81af8c31
Anton Savelev <dpssinet@gmail.com>
2019-06-07 01:34:20 +0300

    Редактирование скриптов

--- js/start.js
+++ js/start.js
@@ -1,26 +1,59 @@
-var renderCloud = function (ctx,x,y,color) {
+'use strict';
+
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+var FONT_GAP = 50;
+var TEXT_WIDTH = 50;
+var COLUMN_GAP = 50;
+// var BAR_HEIGHT = 20;
+// var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
+var BAR_WIDTH = 40;
+var barHeight = CLOUD_HEIGHT - GAP - FONT_GAP - GAP - COLUMN_GAP;
+
+var renderCloud = function(ctx, x, y, color) {
     ctx.fillStyle = color;
-    // ctx.fillRect(x, y)
+    ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+var getMaxElement = function(arr) {
+    var maxElement = arr[0];
+
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] > maxElement) {
+            maxElement = arr[i];
+        }
+    }
+
+    return maxElement;
+};
+
+function getRandomNumber(min, max) {
+    var rand = min + Math.random() * (max + 1 - min);
+    rand = Math.floor(rand);
+    return rand;
 }
 
-window.renderStatistics = function (ctx, names, times) {
-    var MAX_WIDTH_GRAPH = 200;
-    names = ['Вы','Петька','Васька','Танос'];
-    times = ['2000','2500','3000','3200'];
+window.renderStatistics = function(ctx, players, times) {
+    renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
+    renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+    console.log(times);
 
-    // Отрисовка блока
+    ctx.fillStyle = '#000';
 
-    ctx.fillStyle = 'rgba(0,0,0, .7)';
-    ctx.fillRect(110, 20, 420, 270);
+    var maxTime = Math.round(getMaxElement(times));
 
-    ctx.fillStyle = '#fff';
-    ctx.fillRect(100, 10, 420, 270);
+    for (var i = 0; i < players.length; i++) {
 
-    ctx.font = '16px PT Mono';
-    ctx.fillStyle = "black";
-    ctx.fillText('Ура вы победили!' , 130 , 40);
-    ctx.fillText('Список результатов:' , 130 , 60);
-
-
-}
-console.log();
+        ctx.fillStyle = 'black';
+        ctx.fillText(players[i], CLOUD_X + GAP + COLUMN_GAP + FONT_GAP + (GAP + TEXT_WIDTH) * i, CLOUD_HEIGHT + CLOUD_Y - GAP );
+        if(players[i] === 'Вы'){
+            ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+        }else{
+            ctx.fillStyle = 'rgba(0, 0, 255, .'+ getRandomNumber(2,9) +')';
+        }
+        ctx.fillRect(CLOUD_X + GAP + COLUMN_GAP + FONT_GAP + (GAP + TEXT_WIDTH) * i, CLOUD_Y + GAP + CLOUD_HEIGHT - COLUMN_GAP - (barHeight * times[i]) / maxTime , BAR_WIDTH, (barHeight * times[i]) / maxTime);
+    }
+};
